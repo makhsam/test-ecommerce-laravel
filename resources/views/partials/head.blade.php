548edc540ca1a89688d9259040b02e6fb3a12213
Sayidazim Mahmudov <sayidazimahmudov@gmail.com>
2023-07-07 17:29:19 +0500

    npm install & change font size

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Merienda One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     @yield('stylesheet')
 
     <!-- Styles -->
